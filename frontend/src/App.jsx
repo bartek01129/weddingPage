53852b4e3c71c9c71a3973bd217ba1563b8ee189
BartekB <bartek011229@gmail.com>
2026-07-19 19:42:23 +0200

    wylaczenie rsvp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -90,7 +90,7 @@ export default function App() {
 									<Agenda />
 									<Info />
 									<Map />
-									<RSVP />
+									{/* <RSVP /> */}
 									<Gifts />
 									<Contact />
 									<Songs />
